339f6ce3ae60d3b1159af640433297b49cc8efb2
Hen1Qu1 <107431444+Hen1Qu1@users.noreply.github.com>
2023-07-30 17:03:46 -0300

    Add files via upload

---
+++ vbJS/vb-funcoes.js
@@ -0,0 +1,18 @@
+
+function mudaFoto(foto){
+   
+    window.document.getElementById('id-polO').src=foto.value;
+
+}
+
+function mudaFoto2(foto){
+   
+    window.document.getElementById('id-polO-boss').src=foto.value;
+
+}
+
+function mudaFoto3(foto){
+   
+    window.document.getElementById('id-soccer').src=foto.value;
+
+}
